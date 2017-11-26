0c2f707eeba0314c210c434177ad467ec918e163
Adrian Png <adrian.png@fuzziebrain.com>
2017-11-25 16:37:14 -0800

    Implementation with data source

--- src/extension.ts
+++ src/extension.ts
@@ -3,26 +3,24 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { Hover, CompletionItem } from 'vscode';
+import * as path from 'path';
 
-interface PlsqlPackage {
-    packageName: string;
-    methodNames: string[];
+interface ApiDetails {
+  packageName: string,
+  procFuncName: string,
+  bodyNoDefault: string,
+  bodyFullText: string,
+  url: string,
+  descriptionText: string
 }
 
 class PlsqlCompletionItemProvider implements vscode.CompletionItemProvider {
-    private sourceCompletions : CompletionItem[] = new Array<CompletionItem>();
-    private data: Array<PlsqlPackage> = [
-        { packageName: 'dbms_output', methodNames: ['put_line', 'get_line'] },
-        { packageName: 'dbms_crypto', methodNames: ['decrypt', 'encrypt'] }
-    ];
+    private dictionary: Map<string, Map<string, ApiDetails>>;
 
     constructor() {
         // We should load the library here.
-        this.sourceCompletions.push(new CompletionItem('test'));
-        this.sourceCompletions.push(new CompletionItem('dbms_output'));
-        let completionItem = new CompletionItem('dbms_output.put_line', vscode.CompletionItemKind.Function);
-        completionItem.insertText = completionItem.label + '();';
-        this.sourceCompletions.push(completionItem);
+        this.dictionary = require(path.resolve(__dirname, '../data/allFuncProcsOracle12CApex51.json'));
+        console.log('API dictionary successfully loaded.');
     }
 
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
@@ -39,9 +37,9 @@ class PlsqlCompletionItemProvider implements vscode.CompletionItemProvider {
 
         switch(searchTerms.length) {
             case 1:
-                return this.findPackageNames(searchTerms[0]);
+                return this.findPackageByName(searchTerms[0]);
             case 2:
-                return this.findMethodNames(searchTerms[0]);
+                return this.findMethodByName(searchTerms[0]);
             default:
                 return null;
         }
@@ -62,32 +60,38 @@ class PlsqlCompletionItemProvider implements vscode.CompletionItemProvider {
         return text.substring(i + 1, position.character);
     }
 
-    private findPackageNames(searchString: string): CompletionItem[] {
+    private findPackageByName(searchString: string): CompletionItem[] {
         let results: CompletionItem[] = new Array<CompletionItem>();
 
-        this.data.filter(function(plsqlPackage: PlsqlPackage) {
-            return plsqlPackage.packageName.toLowerCase().indexOf(this.toLowerCase()) >= 0;
+        Object.keys(this.dictionary).filter(function(packageName: string) {
+            return packageName.toLowerCase().indexOf(this.toLowerCase()) >= 0;
         }, searchString).forEach(
-            function (value: PlsqlPackage, index: number, array: PlsqlPackage[]) {
-                results.push(new CompletionItem(value.packageName, vscode.CompletionItemKind.Module));
+            function (packageName: string, index: number, array: string[]) {
+                results.push(new CompletionItem(packageName.toLowerCase(), vscode.CompletionItemKind.Module));
             }
         );
 
         return results;
     }
 
-    private findMethodNames(packageName: string): CompletionItem[] {
-        let results: CompletionItem[] = new Array<CompletionItem>();
+    private findMethodByName(packageName: string): CompletionItem[] {
+		let results: CompletionItem[] = new Array<CompletionItem>();
+		let dictionary = this.dictionary;
 
-        this.data.filter(function (plsqlPackage: PlsqlPackage) {
-            return plsqlPackage.packageName.toLowerCase().indexOf(this.toLowerCase()) >= 0;
+        Object.keys(dictionary).filter(function(packageName: string) {
+            return packageName.toLowerCase().indexOf(this.toLowerCase()) >= 0;
         }, packageName).forEach(
-            function (value: PlsqlPackage, index: number, array: PlsqlPackage[]) {
-                value.methodNames.forEach(function (value: string, index: number, array: string[]) {
-                    results.push(new CompletionItem(value, vscode.CompletionItemKind.Method));
+            function (packageName: string, index: number, array: string[]) {
+                Object.keys(dictionary[packageName]).forEach(function (methodName: string, index: number, array: string[]) {
+					let apiDetails: ApiDetails = dictionary[packageName][methodName];
+					let completionItem = new CompletionItem(methodName.toLowerCase(), vscode.CompletionItemKind.Method);
+					completionItem.detail = apiDetails.descriptionText;
+					completionItem.insertText = completionItem.label + '(\n' + apiDetails.bodyNoDefault.replace(/\$(\d+)/g, '\${$1:param}') + '\n);';
+					results.push(completionItem);
                 });
             }
-        );
+		);
+
         return results;
     }
 }
